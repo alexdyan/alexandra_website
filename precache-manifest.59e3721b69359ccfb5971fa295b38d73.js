357789cc6a8e579d46159bf6116c45d2914a79dd
Alexandra <ady239@nyu.edu>
2020-08-14 02:09:28 -0400

    Updates

---
+++ precache-manifest.59e3721b69359ccfb5971fa295b38d73.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717a7842fae698e84eb65bd41934340e",
+    "url": "/alexandra_website/index.html"
+  },
+  {
+    "revision": "09f222162046912918bc",
+    "url": "/alexandra_website/static/css/main.220e526c.chunk.css"
+  },
+  {
+    "revision": "6d1d3aae7ef560e02231",
+    "url": "/alexandra_website/static/js/2.2239d146.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/alexandra_website/static/js/2.2239d146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09f222162046912918bc",
+    "url": "/alexandra_website/static/js/main.80f43438.chunk.js"
+  },
+  {
+    "revision": "e2b63ec5373271ded06d",
+    "url": "/alexandra_website/static/js/runtime-main.692275fd.js"
+  },
+  {
+    "revision": "05860fc3d13dd89a40abd5195bde20cb",
+    "url": "/alexandra_website/static/media/Orbsleep_Alexandra.05860fc3.mp4"
+  },
+  {
+    "revision": "3c4888fdf28846c92db0f3f689dedb61",
+    "url": "/alexandra_website/static/media/Pepsipaint_Alexandra.3c4888fd.mp4"
+  },
+  {
+    "revision": "3b5635ac5d39c28200bf3993d90d09c7",
+    "url": "/alexandra_website/static/media/Raleway-VariableFont_wght.3b5635ac.ttf"
+  },
+  {
+    "revision": "8e599a11c553c7b0db4077891e5045ae",
+    "url": "/alexandra_website/static/media/blackbox.8e599a11.JPG"
+  },
+  {
+    "revision": "f47ebddef92a9cd58cc83083d1b0098b",
+    "url": "/alexandra_website/static/media/home.f47ebdde.svg"
+  },
+  {
+    "revision": "b78dbd664927080bacea5427998ff1a0",
+    "url": "/alexandra_website/static/media/instagram.b78dbd66.svg"
+  },
+  {
+    "revision": "3049877d44c4d8391e792e0c5632d3bf",
+    "url": "/alexandra_website/static/media/linkedin.3049877d.svg"
+  },
+  {
+    "revision": "de2673550ef547982802e98bd855db0a",
+    "url": "/alexandra_website/static/media/orbThumbNail.de267355.JPG"
+  },
+  {
+    "revision": "2dd814d2404a3082395f4056ae26adf9",
+    "url": "/alexandra_website/static/media/pepsiThumbNail.2dd814d2.JPG"
+  },
+  {
+    "revision": "be809f772a8b458add87e268b0a9c0a6",
+    "url": "/alexandra_website/static/media/quarantine.be809f77.png"
+  },
+  {
+    "revision": "cc0938776b85095689f6fa37c1e54f23",
+    "url": "/alexandra_website/static/media/quarantine2.cc093877.JPG"
+  },
+  {
+    "revision": "8adf451d57fc669cc46397cfd68d6426",
+    "url": "/alexandra_website/static/media/soundGarden.8adf451d.JPG"
+  },
+  {
+    "revision": "5cabf9ba564f0ddb56e998fb2d226e56",
+    "url": "/alexandra_website/static/media/tits.5cabf9ba.mp3"
+  },
+  {
+    "revision": "f1a77d1796c02951c867275ee670051d",
+    "url": "/alexandra_website/static/media/tits.f1a77d17.jpg"
+  }
+]);
