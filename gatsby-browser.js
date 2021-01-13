e421f15b055691dbeb04e9659631b49950466459
Quin Scacheri <qs366@nyu.edu>
2021-01-12 22:48:29 -0500

    removed gatsby files

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-const React = require("react")
-const { default: Layout } = require("./src/components/Layout")
-const { default: Theme } = require("./src/components/Theme")
-exports.wrapPageElement = ({ element, props }) => (
-  <Theme {...props}>{element}</Theme>
-)
